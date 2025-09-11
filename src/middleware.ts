223f158ca3cc9842599f422522454a7a898bd649
K4mD4m <KamyszekDamian@outlook.com>
2025-09-11 20:34:07 +0200

    Middleware changed (to prod)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,24 @@
-export { default } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-// Configure which routes require authentication
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  const { pathname } = req.nextUrl;
+
+  // If the user is not authenticated and is trying to access a protected route, redirect to login
+  if (!token && pathname.startsWith("/dashboard")) {
+    const url = req.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
 export const config = {
-  matcher: ["/submit", "/profile"],
+  matcher: [
+    "/dashboard/:path*", // Protect all routes under /dashboard
+  ],
 };
